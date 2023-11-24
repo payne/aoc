f4bd9f0a9d29950c0e2ab24cd851875cdde22a56
Matt Payne <Payne@MattPayne.org>
2023-11-23 22:00:58 -0600

    wip - copied first problem that I had solved in another repo.

--- aoc2022/src/day01/index.ts
+++ aoc2022/src/day01/index.ts
@@ -5,7 +5,7 @@ const parseInput = (rawInput: string) => rawInput;
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return maxCalories(input)
 };
 
 const part2 = (rawInput: string) => {
@@ -13,6 +13,39 @@ const part2 = (rawInput: string) => {
 
   return;
 };
+
+
+export function groupCount(input: any) {
+  let data = input.split("\n\n");
+  return data;
+}
+
+export function calorieArray(input: any) {
+  let groups = groupCount(input);
+  let ca = groups.map(g => {
+    let lines = g.split("\n");
+    let r =  lines.reduce((sum, line) => { 
+      return sum + (1*line); 
+    }, 0);
+    return r;
+  });
+  return ca;
+}
+
+export function maxCalories(input: any) {
+  let ca = calorieArray(input)
+  return Math.max(...ca)
+}
+
+
+export function sumOfTopThree(input: any) {
+  let ca = calorieArray(input)
+  ca.sort()
+  ca.forEach(v => console.log(`\t${v}`));
+  return ca[0] + ca[1] + ca[2];
+}
+
+
 
 run({
   part1: {
