b874d6d4d68f2401a5263b3ff89ae7692a2e3ffc
Matt Payne <Payne@MattPayne.org>
2023-11-24 20:59:34 -0600

    wip - starting to add vitest. yay :-)

---
+++ aoc2022/src/day01/day1a.test.ts
@@ -0,0 +1,23 @@
+// sum.test.ts
+import { expect, test } from 'vitest'
+import { read, groupCount, calorieArray, maxCalories } from './index'
+
+test('can read the file', () => {
+  let contents = read();
+  expect(contents.length).toBe(55)
+})
+
+test('can count the groups', () => {
+  let groups = groupCount();
+  expect(groups.length).toBe(5)
+})
+
+test('calorieArray works with example data', () => {
+  let ca = calorieArray()
+  expect(ca).toStrictEqual([6000, 4000, 11000, 24000, 10000])
+})
+
+test('maxCalories test with sample data', () => {
+ expect(maxCalories()).toBe(24000)
+})
+
