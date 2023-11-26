8e979f1f927fc727ce7d3c9bae5e139391009190
Matt Payne <Payne@MattPayne.org>
2023-11-26 13:39:25 -0600

    wip

---
+++ aoc2022/src/day02/index.test.ts
@@ -0,0 +1,9 @@
+// sum.test.ts
+// var fs = require('fs');
+import * as fs from 'fs';
+import { expect, test } from 'vitest';
+import { read, Rock, Paper, Scissors, play } from './index';
+
+test('Paper covers rock', () => {
+  expect(play(Paper, Rock)).toBe(7);
+})
--- aoc2022/src/day02/index.ts
+++ aoc2022/src/day02/index.ts
@@ -14,6 +14,25 @@ const part2 = (rawInput: string) => {
   return;
 };
 
+export const Rock = Symbol("Rock");
+export const Paper = Symbol("Paper");
+export const Scissors = Symbol("Scissors");
+
+const rules = { Rock: { defeats: Scissors, score: 1  },
+                Scissors: { defeats: Paper, score: 2 }, 
+                Paper: { defeats: Rock, score: 3 }
+             };
+
+export function play(myChoice: any, theirChoice: any) { // TODO: type RPS
+   console.log(rules);
+   const rule = rules[myChoice];
+   console.log(`For myChoice=${myChoice.toString()} rule is ${rule}`);
+   if (myChoice === theirChoice) return 3 + rule.score;
+   if (rule.defeats === theirChoice) return 6 + rule.score;
+   return 0 + rule.score;
+}
+
+
 run({
   part1: {
     tests: [
