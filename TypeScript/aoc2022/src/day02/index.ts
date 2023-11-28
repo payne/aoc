d21add425f6821713aa8fbb2c0c2a5079945985c
Matt Payne <Payne@MattPayne.org>
2023-11-28 15:47:02 -0600

    wip

--- TypeScript/aoc2022/src/day02/index.ts
+++ TypeScript/aoc2022/src/day02/index.ts
@@ -10,8 +10,8 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  const score = strategyGuideScore(input);
+  return score;
 };
 
 export const Rock = "Rock";
@@ -43,6 +43,10 @@ export function play(myChoice: string, theirChoice: string) { // TODO: type RPS
 const codeToChoice:{[key:string]:string} = { 'A': Rock, 'B': Paper, 'C': Scissors, 'X': Rock, 'Y': Paper, 'Z': Scissors};
 
 export function strategyGuideScore(multiLineStragegyGuide: string): number {
+  // 
+  // TODO: loop and add defeatedBy
+  defeatedByMap = new Map();
+  rules.forEach(r => r.defeatedBy = defeatedByMap.get(r));
   const rounds = multiLineStragegyGuide.split('\n');
   return rounds.reduce((total, round) => total + playRound(round), 0);
 }
@@ -51,11 +55,23 @@ function playRound(round: string) {
   if (round.length==0) return 0;
   const parts = round.trim().split(' ').map(s => s.trim())
   const theyPlay = codeToChoice[parts[0]]; // what is a more idiomatic way to do these three steps?
-  const wePlay = codeToChoice[parts[1]];
+  const wePlay = part2XYZlookup(theyPlay,parts[1]);
   if (theyPlay===undefined) { console.log(`round="${round}" Could not find entry for "${parts[0]}"`) }
   if (wePlay===undefined) { console.log(`round="${round}" Could not find entry for "${parts[1]}"`) }
   if (parts[0].length === 0 || parts[1].length ===0 ) return 0;
   return play(wePlay, theyPlay);
+}
+
+function part2XYZlookup(theyPlay: string, goal: string) {
+	switch (goal) {
+		case 'X': // need to loose
+			return roles[theyPlay].defeats;
+		case 'Y': // need to draw
+		  return theyPlay;
+		case 'Z': // need to win
+			return roles[theyPlay].defeatedBy;
+	}
+	return "BUSTED";
 }
 
 run({
