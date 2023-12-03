a170e861f0a31f20bcf3658c41e5293f0665f8dd
Matt Payne <Payne@MattPayne.org>
2023-12-02 20:34:24 -0600

    Now parsing a games reveals

--- TypeScript/aoc2023/src/day02/index.ts
+++ TypeScript/aoc2023/src/day02/index.ts
@@ -17,10 +17,28 @@ const part2 = (rawInput: string) => {
 export interface Balls { blue, red, green: number }
 
 export function countBalls(line: string) : Balls[] {
+ const reveals = line.substring(line.indexOf(':')+1).split(';')
+ console.log(reveals);
+ const balls = reveals.map(r => normalizeReveal(r));
+ console.log(balls);
+ return balls;
+ /*
  return [ { blue: 3, red: 4, green: 0 }, 
           { blue: 0, red: 0, green: 0 },
           { blue: 0, red: 0, green: 0 }
         ];
+ */
+}
+function normalizeReveal(reveal: string): Ball {
+  // reveal is like:
+  // ' 3 blue, 4 red' 
+  const pairs = reveal.trim().split(',');
+         const ball: Ball= { blue: 0, red: 0, green: 0 };
+  pairs.forEach(p => {
+    const [numStr, color] = p.trim().split(' ')
+    ball[color] = 1*numStr;
+  });
+  return ball;
 }
 
 run({
